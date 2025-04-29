adcb41cc67eb0c36a1bb8e8dfa761931624ca96f
RiyasTp <mohammed.riyas@aj-ex.com>
2025-04-30 04:30:03 +0530

    added audio player , need to improve cache and scroll effect

--- lib/features/al_quran/screens/sura_aya_wise_screen.dart
+++ lib/features/al_quran/screens/sura_aya_wise_screen.dart
@@ -1,14 +1,15 @@
-
 import 'package:al_quran/features/al_quran/models/quran_models.dart';
 import 'package:al_quran/features/al_quran/screens/sheets_and_alerts/copy_aya_bottom_sheet.dart';
 import 'package:al_quran/features/al_quran/widgets/aya_rich_text_widget.dart';
 import 'package:al_quran/features/al_quran/widgets/bismillah_widget.dart';
 import 'package:al_quran/features/al_quran/widgets/constant_widgets.dart';
 import 'package:al_quran/features/al_quran/widgets/custom_icon_button.dart';
+import 'package:al_quran/features/audio_plyer/quran_audio_player.dart';
 import 'package:al_quran/features/bookmarks/book_marks_db_helper.dart';
 import 'package:al_quran/features/notes/db_helper.dart';
 import 'package:al_quran/features/notes/edit_notes_bottom_sheet.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
 
 class AyahPage extends StatefulWidget {
@@ -28,167 +29,241 @@ class AyahPage extends StatefulWidget {
   State<AyahPage> createState() => _AyahPageState();
 }
 
-
-
 class _AyahPageState extends State<AyahPage> {
   final _dbHelper = BookmarkDatabaseHelper.instance;
   final _notesDbHelper = NotesDatabaseHelper.instance;
   final ItemScrollController itemScrollController = ItemScrollController();
+
+  late final QuranAudioPlayer audioPlayer;
+
   @override
   void initState() {
     super.initState();
-    Future.delayed(Duration.zero, () {
-      final initialIndex = widget.initialAyahIndex != null
-          ? widget.sura.ayas
-              .indexWhere((aya) => aya.index == widget.initialAyahIndex)
-          : 0;
-      if (initialIndex > 0) {
-        itemScrollController.jumpTo(index: initialIndex);
+    audioPlayer = QuranAudioPlayer();
+
+    audioPlayer.init();
+    audioPlayer.addListener(() {
+      if (audioPlayer.isPlaying) {
+        _scrollToAyah(audioPlayer.currentAyyaIndex);
       }
     });
+
+    final initialIndex = widget.initialAyahIndex != null
+        ? widget.sura.ayas
+            .indexWhere((aya) => aya.index == widget.initialAyahIndex)
+        : 0;
+    if (initialIndex > 0) {
+      _jumpToAyah(audioPlayer.currentAyyaIndex);
+    }
+  }
+
+  Future<void> _playSurahFromStart() async {
+    await audioPlayer.playSurah(widget.sura);
+  }
+
+  Future<void> _playAya(Aya aya, int index) async {
+    audioPlayer.playSurah(widget.sura, startAya: index);
+  }
+
+  @override
+  void dispose() {
+    audioPlayer.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        title: RichText(
-          text: TextSpan(
-            style: Theme.of(context).textTheme.titleLarge,
-            children: [
-              TextSpan(
-                text: '${widget.sura.index.toString()}. ',
-              ),
-              TextSpan(text: '${widget.suraMetaData.tname}|'),
-              TextSpan(
-                  text: widget.sura.name,
-                  style: TextStyle(fontWeight: FontWeight.bold)),
-            ],
-          ),
-        ),
-        actions: [
-          IconButton(onPressed: () {}, icon: Icon(Icons.settings_outlined))
-        ],
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 18.0),
-        child: ScrollablePositionedList.builder(
-          itemScrollController: itemScrollController,
-          itemBuilder: (context, index) {
-            final aya = widget.sura.ayas[index];
-            final ayaTranslation = widget.suraTranslation.ayas[index];
-
-            var richText = AyaRichText(aya: aya);
-
-            return Column(
-              crossAxisAlignment: CrossAxisAlignment.end,
+    return ChangeNotifierProvider(
+      create: (context) => audioPlayer,
+      // builder: (context, child) => AnnotatedRegion<SystemUiOverlayStyle>(
+      //   value: SystemUiOverlayStyle.light,
+      //   child: WillPopScope(
+      //     onWillPop: () async {
+      //       audioPlayer.stop();
+      //       return true;
+      //     },
+      //     child: _buildAyahPage(context),
+      //   ),
+      // ),
+      child: Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          title: RichText(
+            text: TextSpan(
+              style: Theme.of(context).textTheme.titleLarge,
               children: [
-                const SizedBox(height: 10),
-                if (aya.bismillah != null) BismillahWidget(),
-                richText,
-                Text(ayaTranslation.text),
-                const SizedBox(height: 4),
-                Container(
-                  margin: EdgeInsets.symmetric(vertical: 4),
-                  decoration: BoxDecoration(
-                      color: Theme.of(context).colorScheme.secondaryContainer,
-                      borderRadius: BorderRadius.all(Radius.circular(8))),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      CustomIconButton(
-                        child: Text(aya.index.toString(),
-                            style: Theme.of(context)
-                                .textTheme
-                                .bodyMedium
-                                ?.copyWith(
-                                  color: Theme.of(context).colorScheme.primary,
-                                )),
-                      ),
-                      Row(
-                        children: [
-                          CustomIconButton(
-                            child: Icon(
-                              Icons.info_outline_rounded,
-                              size: 22,
-                              color: Theme.of(context).colorScheme.primary,
-                            ),
-                          ),
-                          CustomIconButton(
-                            child: Icon(
-                              Icons.copy,
-                              size: 22,
-                              color: Theme.of(context).colorScheme.primary,
-                            ),
-                            onTap: () {
-                              showCopyBottomSheet(
-                                context,
-                                arabicText: aya.text,
-                                translationText: ayaTranslation.text,
-                                reference:
-                                    "Surah ${widget.sura.name} • ${widget.sura.index} ${aya.index}",
-                              );
-                            },
-                          ),
-                          CustomIconButton(
-                            child: FutureBuilder<bool>(
-                                future: _notesDbHelper.hasNoteForAyah(
-                                    widget.sura.index, aya.index),
-                                builder: (context, snapshot) {
-                                  final hasNote = snapshot.data ?? false;
-                                  return Icon(
-                                    hasNote
-                                        ? Icons.sticky_note_2_rounded
-                                        : Icons.sticky_note_2_outlined,
-                                    size: 22,
+                TextSpan(
+                  text: '${widget.sura.index.toString()}. ',
+                ),
+                TextSpan(text: '${widget.suraMetaData.tname}|'),
+                TextSpan(
+                    text: widget.sura.name,
+                    style: TextStyle(fontWeight: FontWeight.bold)),
+              ],
+            ),
+          ),
+          actions: [
+            IconButton(onPressed: () {}, icon: Icon(Icons.settings_outlined))
+          ],
+        ),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 18.0),
+          child: ScrollablePositionedList.builder(
+            itemScrollController: itemScrollController,
+            itemBuilder: (context, index) {
+              final aya = widget.sura.ayas[index];
+              final ayaTranslation = widget.suraTranslation.ayas[index];
+
+              var richText = AyaRichText(aya: aya);
+
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  const SizedBox(height: 10),
+                  if (aya.bismillah != null) BismillahWidget(),
+                  richText,
+                  Text(ayaTranslation.text),
+                  const SizedBox(height: 4),
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 4),
+                    decoration: BoxDecoration(
+                        color: Theme.of(context).colorScheme.secondaryContainer,
+                        borderRadius: BorderRadius.all(Radius.circular(8))),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        CustomIconButton(
+                          child: Text(aya.index.toString(),
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .bodyMedium
+                                  ?.copyWith(
                                     color:
                                         Theme.of(context).colorScheme.primary,
+                                  )),
+                        ),
+                        Row(
+                          children: [
+                            CustomIconButton(
+                              child: Icon(
+                                Icons.info_outline_rounded,
+                                size: 22,
+                                color: Theme.of(context).colorScheme.primary,
+                              ),
+                            ),
+                            CustomIconButton(
+                              child: Icon(
+                                Icons.copy,
+                                size: 22,
+                                color: Theme.of(context).colorScheme.primary,
+                              ),
+                              onTap: () {
+                                showCopyBottomSheet(
+                                  context,
+                                  arabicText: aya.text,
+                                  translationText: ayaTranslation.text,
+                                  reference:
+                                      "Surah ${widget.sura.name} • ${widget.sura.index} ${aya.index}",
+                                );
+                              },
+                            ),
+                            CustomIconButton(
+                              child: FutureBuilder<bool>(
+                                  future: _notesDbHelper.hasNoteForAyah(
+                                      widget.sura.index, aya.index),
+                                  builder: (context, snapshot) {
+                                    final hasNote = snapshot.data ?? false;
+                                    return Icon(
+                                      hasNote
+                                          ? Icons.sticky_note_2_rounded
+                                          : Icons.sticky_note_2_outlined,
+                                      size: 22,
+                                      color:
+                                          Theme.of(context).colorScheme.primary,
+                                    );
+                                  }),
+                              onTap: () => _handleNoteAction(aya.index),
+                            ),
+                            FutureBuilder<bool>(
+                                future: _dbHelper.isBookmarked(
+                                  type: 'ayah',
+                                  surahNumber: widget.sura.index,
+                                  ayahNumber: aya.index,
+                                ),
+                                builder: (context, snapshot) {
+                                  final isBookmarked = snapshot.data ?? false;
+                                  return CustomIconButton(
+                                    child: Icon(
+                                      isBookmarked
+                                          ? Icons.bookmark
+                                          : Icons.bookmark_border,
+                                      size: 22,
+                                      color:
+                                          Theme.of(context).colorScheme.primary,
+                                    ),
+                                    onTap: () => _toggleAyahBookmark(aya.index),
                                   );
                                 }),
-                            onTap: () => _handleNoteAction(aya.index),
-                          ),
-                          FutureBuilder<bool>(
-                              future: _dbHelper.isBookmarked(
-                                type: 'ayah',
-                                surahNumber: widget.sura.index,
-                                ayahNumber: aya.index,
-                              ),
-                              builder: (context, snapshot) {
-                                final isBookmarked = snapshot.data ?? false;
+                            Consumer<QuranAudioPlayer>(
+                              builder: (context, audioPlayer, child) {
                                 return CustomIconButton(
                                   child: Icon(
-                                    isBookmarked
-                                        ? Icons.bookmark
-                                        : Icons.bookmark_border,
+                                    audioPlayer.currentAyyaIndex == index &&
+                                            audioPlayer.isPlaying
+                                        ? Icons.pause_circle_outline
+                                        : Icons.play_circle_outline,
                                     size: 22,
                                     color:
                                         Theme.of(context).colorScheme.primary,
                                   ),
-                                  onTap: () => _toggleAyahBookmark(aya.index),
+                                  onTap: () {
+                                    if (audioPlayer.currentAyyaIndex == index &&
+                                        audioPlayer.isPlaying) {
+                                      audioPlayer.pause();
+                                    } else {
+                                      _playAya(aya, index);
+                                    }
+                                  },
                                 );
-                              }),
-                          CustomIconButton(
-                            child: Icon(
-                              Icons.play_circle_outline_rounded,
-                              size: 22,
-                              color: Theme.of(context).colorScheme.primary,
+                              },
                             ),
-                          ),
-                        ],
-                      )
-                    ],
+                          ],
+                        )
+                      ],
+                    ),
                   ),
-                ),
-                divider,
-                const SizedBox(height: 10),
-              ],
-            );
-          },
-          itemCount: widget.sura.ayas.length,
+                  divider,
+                  const SizedBox(height: 10),
+                ],
+              );
+            },
+            itemCount: widget.sura.ayas.length,
+          ),
         ),
       ),
     );
+  }
+
+  void _jumpToAyah(int ayahIndex) {
+    Future.delayed(Duration.zero, () {
+      if (ayahIndex >= 0 && ayahIndex < widget.sura.ayas.length) {
+        itemScrollController.jumpTo(
+          index: ayahIndex,
+        );
+      }
+    });
+  }
+
+  void _scrollToAyah(int ayahIndex) {
+    Future.delayed(Duration.zero, () {
+      if (ayahIndex >= 0 && ayahIndex < widget.sura.ayas.length) {
+        itemScrollController.scrollTo(
+          duration: const Duration(milliseconds: 500),
+          curve: Curves.easeInOut,
+          index: ayahIndex,
+        );
+      }
+    });
   }
 
   Future<void> _toggleAyahBookmark(int ayahNumber) async {
@@ -229,4 +304,3 @@ class _AyahPageState extends State<AyahPage> {
     setState(() {}); // Refresh the UI
   }
 }
-
---
+++ lib/features/audio_plyer/quran_audio_player.dart
@@ -0,0 +1,113 @@
+import 'dart:async';
+
+import 'package:al_quran/features/al_quran/models/quran_models.dart';
+import 'package:audio_session/audio_session.dart';
+import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
+
+class QuranAudioPlayer extends ChangeNotifier {
+  final AudioPlayer _audioPlayer = AudioPlayer();
+  static final QuranAudioPlayer _instance = QuranAudioPlayer._internal();
+  factory QuranAudioPlayer() => _instance;
+  QuranAudioPlayer._internal();
+  static QuranAudioPlayer get instance => _instance;
+  
+  static const String _baseUrl = 'https://tanzil.net/res/audio/';
+  String author = 'afasy';
+  List<String> authors = ['afasy', 'mishary'];
+  static const String _audioFormat = '.mp3';
+  
+  Sura? _currentSura;
+  List<Aya>? _currentAyas;
+  int _currentAyaIndex = 0;
+  get currentAyyaIndex => _currentAyaIndex;
+  StreamSubscription<PlayerState>? _playerStateSubscription;
+  
+  Future<void> init() async {
+    final session = await AudioSession.instance;
+    await session.configure(const AudioSessionConfiguration.music());
+  }
+  get isPlaying => _audioPlayer.playing;
+  
+  Future<void> playSurah(Sura sura, {int startAya = 0}) async {
+    // Clear previous subscription
+    await _playerStateSubscription?.cancel();
+    _playerStateSubscription = null;
+
+    // Stop any current playback
+    await _audioPlayer.stop();
+
+    // Set new surah data
+    _currentSura = sura;
+    _currentAyas = sura.ayas;
+    _currentAyaIndex = startAya;
+    
+    await _playCurrentAya();
+  }
+
+  Future<void> _playCurrentAya() async {
+    if (_currentSura == null || 
+        _currentAyas == null || 
+        _currentAyaIndex >= _currentAyas!.length) {
+      return;
+    }
+
+    final aya = _currentAyas![_currentAyaIndex];
+    final audioUrl = _getAyaAudioUrl(
+      _currentSura!.index.toString(),
+      aya.index.toString(),
+    );
+
+    try {
+      // Cancel previous subscription if exists
+      await _playerStateSubscription?.cancel();
+
+      await _audioPlayer.setAudioSource(AudioSource.uri(Uri.parse(audioUrl)));
+      
+      // Create new subscription
+      _playerStateSubscription = _audioPlayer.playerStateStream.listen((state) {
+        notifyListeners();
+        if (state.processingState == ProcessingState.completed) {
+          _playNextAya();
+        }
+      });
+
+      await _audioPlayer.play();
+
+    } catch (e) {
+      print('Error playing audio: $e');
+    }
+  }
+
+  Future<void> _playNextAya() async {
+    _currentAyaIndex++;
+    notifyListeners();
+    if (_currentAyaIndex < _currentAyas!.length) {
+      await _playCurrentAya();
+    }
+  }
+
+  String _getAyaAudioUrl(String suraIndex, String ayaIndex) {
+    return "${_baseUrl}afasy/${suraIndex.padLeft(3, '0')}${ayaIndex.padLeft(3, '0')}$_audioFormat";
+  }
+
+  Future<void> pause() async {
+    await _audioPlayer.pause();
+    notifyListeners();
+  }
+
+  Future<void> stop() async {
+    await _playerStateSubscription?.cancel();
+    _playerStateSubscription = null;
+    await _audioPlayer.stop();
+    notifyListeners();
+  }
+
+  @override
+  Future<void> dispose() async {
+    await _playerStateSubscription?.cancel();
+    _playerStateSubscription = null;
+    await _audioPlayer.dispose();
+    super.dispose();
+  }
+}
